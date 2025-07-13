1ebde99768b4d92635a638608162c2e4f246cece
MageshGowtham5 <mageshgowtham3@gmail.com>
2025-07-13 23:49:38 +0530

    LC no:122)Best Time to Buy and Sell Stock II:

--- Main.java
+++ Main.java
@@ -111,3 +111,16 @@
             return max;
         }
     }
+
+//8.(LC no:122)Best Time to Buy and Sell Stock II:
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int profit=0;
+            for(int i=1;i<prices.length;i++){
+                if(prices[i-1]<prices[i]){
+                    profit=profit+prices[i]-prices[i-1];
+                }
+            }
+            return profit;
+        }
+    }
