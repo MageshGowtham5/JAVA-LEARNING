9231d80fa0e8d63a4ebd1fec8923909d2bdea0ea
MageshGowtham5 <mageshgowtham3@gmail.com>
2025-08-07 11:37:06 +0530

    Solved Roman to integer

--- String_Array.java
+++ String_Array.java
@@ -152,3 +152,26 @@
             }return jump;
         }
     }
+
+//11.(LC no:13)Roman to Integer:
+    class Solution {
+        public int romanToInt(String s) {
+            int val=0;
+            HashMap<Character,Integer> roman=new HashMap<>();
+            roman.put('I',1);
+            roman.put('V',5);
+            roman.put('X',10);
+            roman.put('L',50);
+            roman.put('C',100);
+            roman.put('D',500);
+            roman.put('M',1000);
+            for(int i=s.length()-2;i>=0;i--){
+                if(roman.get(s.charAt(i))<roman.get(s.charAt(i+1))){
+                    val-=roman.get(s.charAt(i));
+                }else{
+                    val+=roman.get(s.charAt(i));
+                }
+            }
+            return val+roman.get(s.charAt(s.length()-1));
+        }
+    }
